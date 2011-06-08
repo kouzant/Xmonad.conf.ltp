bc14ff19412069cbb6423c2d90f1e6d52f5fc64e
alafroiskiotos <praetorian@akyroi.gr>
2011-06-08 23:56:20 +0300

    commit

---
+++ sync.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import shutil
+import os
+
+if __name__=="__main__":
+    #Home Directory
+    home=os.getenv("HOME")
+    #Repository name
+    repo=home+"/xmonad.conf.ltp/"
+    #Directories to copy
+    #SOURCE:TARGET
+    dirs={home+"/.xmonad/":".xmonad/",
+            home+"/scripts/xmobar/":"scripts/xmobar/"}
+    #Files to copy    
+    files=(home+"/.Xdefaults",
+            home+"/.xmobarrc",
+            home+"/.xsession")
+
+    #First copy the directories
+    for dir,name in dirs.items():
+        repoDir=repo+name
+        #shutli.copytree() needs the target directory
+        #not to exist
+        if os.path.exists(repoDir):
+            shutil.rmtree(repoDir)
+        shutil.copytree(dir,repoDir)
+
+    #Copy the files
+    for file in files:
+        shutil.copy2(file,repo)
